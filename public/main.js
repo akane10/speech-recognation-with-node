74cdb570d2d82598153b2386ec85819bb7bbfc00
yapie <targaryen.akane@gmail.com>
2018-08-21 15:16:07 +0700

    commit

---
+++ public/main.js
@@ -0,0 +1,29 @@
+const video = document.querySelector('#video');
+
+let remoteControl = new RemoteControl(video);
+let recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = "en-US";
+recognition.continuous = true;
+recognition.start();
+
+recognition.onresult = function(event) {
+  for (let i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      if (event.results[i][0].transcript.trim() == "play") {
+        remoteControl.play();
+      }
+      if (event.results[i][0].transcript.trim() == "stop") {
+        remoteControl.stop();
+      }
+      if (event.results[i][0].transcript.trim() == "mute") {
+        remoteControl.mute();
+      }
+      if (event.results[i][0].transcript.trim() == "unmute") {
+        remoteControl.unmute();
+      }
+      console.info(`You said : ${event.results[i][0].transcript}`);
+    }
+  }
+};
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('on port ' +3000));
+app.get('/', (req, res) => res.sendFile(__dirname+'/public/index.html'));
